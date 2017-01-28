7e770b4cbe9783340b0bf67ea4d429448f1d229e
adam359 <adam359@hawaii.edu>
2017-01-27 19:26:36 -1000

    ics313 assignment 2 complete

---
+++ spring17/ics313/LandofLisp/header.lisp
@@ -0,0 +1,23 @@
+;;;; -*- Mode: LISP; Syntax: Common-lisp; Package: USER; Base: 10 -*-                                  ;;;; Name:                                          Date:                                              ;;;; Course: ICS313        Assignment:                                                                 ;;;; File: header.lisp                                                                                                                     
+
+(in-package :User) ; optional - this is also in the header above                                                                           
+                                                                                               
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
