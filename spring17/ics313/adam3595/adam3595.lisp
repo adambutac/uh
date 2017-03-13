13a84b213e85f45f1b5a4c0ec7fce53db628445b
Adam359 <adam359@hawaii.edu>
2017-03-12 16:28:56 -1000

    finished ics313 assg 5

--- spring17/ics313/adam3595/adam3595.lisp
+++ spring17/ics313/adam3595/adam3595.lisp
@@ -4,12 +4,6 @@
 ;;;; File: adam3595.lisp
 (in-package :User) ; optional - this is also in the header above
 (defconstant +ID+ "Adam Butac")
-;;;; -*- Mode: LISP; Syntax: Common-lisp; Package: USER; Base: 10 -*-
-;;;; Name: Adam Butac                               Date:Fri 24 Feb 2017 02:07:49 PM HST
-;;;; File: adam3594B.lisp
-
-(in-package :User) ; optional - this is also in the header above
-(defconstant +ID+ "Adam Butac")
 
 (defun id (crsNum asnNum)
   "Print name, course number, and homework assignment number. Return nil."
@@ -17,62 +11,36 @@
     ((and (integerp crsNum) (integerp asnNum))
       (format t "Name: ~S~%" +ID+)
       (format t "Course: ICS~D~%" crsNum)
-      (format t "Assignment # ~D~%" asnNum)))
-)
+      (format t "Assignment # ~D~%" asnNum))))
 
 ; wizards_game part 1
 (defparameter *nodes* '())
 
 (defmacro new-location (loc des)
+  "macro to create new locations in the game"
   (if (and (symbolp loc) (consp des))
     (if (not (assoc loc *nodes*))
       `(setq *nodes* (append *nodes* '((,loc ,des))))
-      (format t "~S already exists in the game.~%" loc)
-    )
-    (format t "Incorrect parameter types.  Macro new-location requires a symbol and a list.")
-  )
-)
-
-(new-location living-room (you are in the living-room.
-                                a wizard is snoring loudly on the couch.))
-(new-location garden (you are in a beautiful garden.
-                           there is a well in front of you.))
-(new-location attic (you are in the attic.
-                          there is a giant welding torch in the corner.))
-(new-location kitchen (you are in the kitchen.
-                            there is a beautiful chocolate cake on the table.))
+      (format t "~S already exists in the game.~%" loc))
+    (format t "Incorrect parameter types.  Macro new-location requires a symbol and a list.")))
 
 (defun describe-location (location nodes)
    (cadr (assoc location nodes)))
 
 (defparameter *edges* '())
-(defmacro new-edge (loc edgs &optional opt1 opt2)
+(defmacro new-path (loc edgs &optional opt1 opt2)
+  "macro designed to create edges going from one location to another"
   (if (and (symbolp loc) (consp edgs))
     (if (not (assoc loc *edges*))
       (cond
         ((and (null opt1) (null opt2))
-          `(push  '(,loc ,edgs) *edges*)
-        )
+          `(push  '(,loc ,edgs) *edges*))
         ((null opt2)
-          `(push  '(,loc ,edgs (,@opt1)) *edges*)
-        )
+          `(push  '(,loc ,edgs (,@opt1)) *edges*))
         (t
-          `(push  '(,loc ,edgs (,@opt1) (,@opt2)) *edges*)
-        )
-      )
-    )
-    (format t "Incorrect parameter types.  Macro new-edge requires a symbol and a list.")
-  )
-)
-
-(new-edge living-room (garden west door)
-                      (attic upstairs ladder)
-                      (kitchen north door))
-(new-edge garden (living-room east door))
-(new-edge attic (living-room downstairs ladder))
-(new-edge kitchen (living-room south door))
-
-
+          `(push  '(,loc ,edgs (,@opt1) (,@opt2)) *edges*)))
+      (format t "Paths from ~s were already generated.~%" loc))
+    (println "Incorrect parameter types.  Macro new-path requires a symbol and a list.")))
 
 (defun describe-path (edge)
   `(there is a ,(caddr edge) going ,(cadr edge) from here.))
@@ -84,38 +52,20 @@
 (defparameter *object-locations* ())
 
 (defmacro println (msg)
-  (format t "~s~%" msg)
-)
+  "lets you print a line followed by a new line"
+  (format t "~s~%" msg))
 
 (defmacro new-object (loc obj)
-  (setq objAtLoc (eq (car (assoc loc *object-locations*)) obj))
+  "macro designed to create and place objects into the game"
   (cond
-    ((null loc)
-      (println "location is null.")
-    )
-    ((null obj)
-      (println "object is null.")
-    )
-    (objAtLoc
-      (format t "~s exists at ~s.~%" obj loc)
-    )
+    ((null loc))
+    ((null obj))
+    ((member loc (assoc obj *object-locations*))
+      (format t "~s already exists in ~s.~%" obj loc))
     (t
       (push obj *objects*)
-      (push `(,obj ,loc) *object-locations*)
-    )
-  )
-  (format t "~s~%" (rassoc loc *object-locations*))
-  (format t "~s~%" (assoc obj *objects*))
-)
-
-(new-object living-room chair)
-(new-object living-room table)
-(new-object living-room couch)
-(new-object basement book)
-(new-object basement book)
-
-(format t "ITEMS:~s~%" *objects*)
-(format t "ITEM-LOCATIONS:~s~%" *object-locations*)
+      (push `(,obj ,loc) *object-locations*)))
+  nil)
 
 (defun objects-at (loc objs obj-loc)
    (labels ((is-at (obj)
@@ -134,7 +84,6 @@
           (describe-paths *location* *edges*)
           (describe-objects *location* *objects* *object-locations*)))
 
-
 (defun walk (direction)
   (labels ((correct-way (edge)
              (eq (cadr edge) direction)))
@@ -150,13 +99,24 @@
          `(you are now carrying the ,object))
       (t '(you cannot get that.))))
 
-
 (defun inventory ()
   (cons 'items- (objects-at 'body *objects* *object-locations*)))
 
 (defun have (object)
     (member object (cdr (inventory))))
 ;  wizards_game part 2
+
+(defun help ()
+  "lists the allowed commands"
+  (game-print (append '(try- ) *allowed-commands*)))
+
+(defun h ()
+  "lists the allowed commands"
+  (help))
+
+(defun ? ()
+  "lists the allowed commands"
+  (help))
 
 (defun game-repl ()
     (let ((cmd (game-read)))
@@ -170,7 +130,7 @@
                     (list 'quote x)))
              (cons (car cmd) (mapcar #'quote-it (cdr cmd))))))
 
-(defparameter *allowed-commands* '(look walk pickup inventory))
+(defparameter *allowed-commands* '(look walk pickup inventory help h ?))
 
 (defun game-eval (sexp)
     (if (member (car sexp) *allowed-commands*)
@@ -192,3 +152,84 @@
     (princ (coerce (tweak-text (coerce (string-trim "() " (prin1-to-string lst)) 'list) t nil) 'string))
     (fresh-line))
 
+(defmacro game-action (command subj obj place &body body)
+  `(progn (defun ,command (subject object)
+            (if (and (eq *location* ',place)
+                     (eq subject ',subj)
+                     (eq object ',obj)
+                     (have ',subj))
+                ,@body
+            '(i cant ,command like that.)))
+          (pushnew ',command *allowed-commands*)))
+
+(defparameter *chain-welded* nil)
+
+(game-action weld chain bucket attic
+             (if (and (have 'bucket) (not *chain-welded*))
+                 (progn (setf *chain-welded* 't)
+                        '(the chain is now securely welded to the bucket.))
+               '(you do not have a bucket.)))
+
+(defparameter *bucket-filled* nil)
+
+(game-action dunk bucket well garden
+             (if *chain-welded*
+                 (progn (setf *bucket-filled* 't)
+                        '(the bucket is now full of water))
+               '(the water level is too low to reach.)))
+
+(game-action splash bucket wizard living-room
+             (cond ((not *bucket-filled*) '(the bucket has nothing in it.))
+                   ((have 'frog) '(the wizard awakens and sees that you stole his frog.
+                                   he is so upset he banishes you to the
+                                   netherworlds- you lose! the end.))
+                   (t '(the wizard awakens from his slumber and greets you warmly.
+                        he hands you the magic low-carb donut- you win! the end.))))
+
+(defparameter *pi-baked* nil)
+
+(game-action bake pie oven kitchen
+  "includes the bake command to let the user bake a pie"
+  (if (and (have 'raspberry)
+           (have 'ibm-keyboard)
+           (have 'crt-monitor)
+           (have 'pie)
+           (not *pi-baked*))
+    (progn
+      (setf *pi-baked* 't)
+      (game-print '(you put the raspberry ibm-keyboard crt-monitor and pie into the oven
+            and set it to ez-bake mode. *ding* out pops a thinkpad! You are hypnotized by
+            its boot animation... an unknown force guides your hands as you furiously type
+            in an ancient and archaech language. all of a sudden...))
+      (load "adam3595.lisp")
+      (game-repl)
+      (game-print (look)))
+    '(you are missing some items...)))
+
+;; add all the locations to the game
+(new-location living-room (you are in the living-room.
+                                a wizard is snoring loudly on the couch.))
+(new-location garden (you are in a beautiful garden.
+                           there is a well in front of you.))
+(new-location attic (you are in the attic.
+                          there is a giant welding torch in the corner.))
+(new-location kitchen (you are in the kitchen.
+                            there is a beautiful chocolate cake on the table.
+                            there is an oven in front of you.))
+;; add all the paths to the game
+(new-path living-room (garden west door)
+                      (attic upstairs ladder)
+                      (kitchen north door))
+(new-path garden (living-room east door))
+(new-path attic (living-room downstairs ladder))
+(new-path kitchen (living-room south door))
+;; add all the objects to the game
+(new-object living-room crt-monitor)
+(new-object living-room whiskey)
+(new-object living-room bucket)
+(new-object attic ibm-keyboard)
+(new-object garden raspberry)
+(new-object garden chain)
+(new-object garden frog)
+(new-object kitchen cake)
+(new-object kitchen pie)
